c50b593aa6dda9c1f31b426eb1bb9662074607d7
Kazuhiro Nakamura <kazuhironakamur@yahoo.co.jp>
2019-04-02 19:18:20 +0900

    DBの本番向け設定を追加

--- taby/settings.py
+++ taby/settings.py
@@ -98,12 +98,15 @@ else:
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': 'taby',
-            'USER': 'wakoit',
+            'USER': 'taby',
             'PASSWORD': 'NOVA=324929',
             'HOST': 'localhost',
             'PORT': '5432',
         }
     }
+
+    db_from_env = dj_database_url.config(conn_max_age=500)
+    DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
