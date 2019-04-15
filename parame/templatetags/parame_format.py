f8ca2cdfe48c3060218e2a34973896379414f19e
Kazuhiro Nakamura <nakamura_k@agricom.co.jp>
2019-04-15 13:10:54 +0900

    参考URLを追加

--- parame/templatetags/parame_format.py
+++ parame/templatetags/parame_format.py
@@ -7,6 +7,9 @@ def cut(value, arg):
     "入力から arg の値を全て取り去る"
     return value.replace(arg, '')
 
+"""
+https://djangoproject.jp/doc/ja/1.0/howto/custom-template-tags.html
+"""
 @register.filter
 def tab(value):
     return mark_safe('<table border="1"><thead><th></th></thead><tbody><tr><td>' + value + '</td></tr></tbody></table>')
