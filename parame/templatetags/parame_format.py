6feef2c2e74e628294ee78b4f4d3f38d9346e071
Kazuhiro Nakamura <nakamura_k@agricom.co.jp>
2019-04-15 17:34:22 +0900

    valueのesc処理サンプルを追加。

--- parame/templatetags/parame_format.py
+++ parame/templatetags/parame_format.py
@@ -1,8 +1,9 @@
 from django import template
-from django.utils.safestring import mark_safe
 
 register = template.Library()
 
+from django.utils.html import conditional_escape
+from django.utils.safestring import mark_safe
 def cut(value, arg):
     "入力から arg の値を全て取り去る"
     return value.replace(arg, '')
@@ -12,4 +13,8 @@ https://djangoproject.jp/doc/ja/1.0/howto/custom-template-tags.html
 """
 @register.filter
 def tab(value):
-    return mark_safe('<table border="1"><thead><th></th></thead><tbody><tr><td>' + value + '</td></tr></tbody></table>')
+    esc = conditional_escape
+
+    return mark_safe('<table border="1"><thead><th></th></thead><tbody><tr><td>' +
+        esc(value) +
+        '</td></tr></tbody></table>')
