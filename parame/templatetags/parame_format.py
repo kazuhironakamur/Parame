14962c2a07d72321a8ecc7e84d2519b884f8428b
Kazuhiro Nakamura <nakamura_k@agricom.co.jp>
2019-04-15 13:09:06 +0900

    テーブル化のサンプルコードを追加

--- parame/templatetags/parame_format.py
+++ parame/templatetags/parame_format.py
@@ -1,7 +1,12 @@
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
 def cut(value, arg):
     "入力から arg の値を全て取り去る"
     return value.replace(arg, '')
+
+@register.filter
+def tab(value):
+    return mark_safe('<table border="1"><thead><th></th></thead><tbody><tr><td>' + value + '</td></tr></tbody></table>')
