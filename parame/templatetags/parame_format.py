c62393bdc4d614410c285202494d2c38314a2822
Kazuhiro Nakamura <kazuhironakamur@yahoo.co.jp>
2019-04-16 21:06:34 +0900

    字下げとclass適用をエイヤで片づけた

--- parame/templatetags/parame_format.py
+++ parame/templatetags/parame_format.py
@@ -18,22 +18,37 @@ def tab(value):
         if max_cols_count < cols_count:
             max_cols_count = cols_count
 
-    table = '<table class="table table-sm table-bordered table-hover">'
-    table += '<thead class="thead-light"><th colspan="' + str(max_cols_count) +  '">Parameter Sheet</th><thead>'
-    table += '<tbody>'
+    table = '<table class="table table-sm table-bordered table-hover">\n'
+    table += '<thead class="thead-light"><th colspan="' + str(max_cols_count) +  '">Parameter Sheet</th><thead>\n'
+    table += '<tbody>\n'
     for line in esc(value).splitlines():
         cols_count = len(line.split('|'))
         elm = []
         for col in line.split('|'):
             span = max_cols_count - cols_count + 1
             if span > 1:
-                elm.append('<td colspan="' + str(span) + '">' + col + '</td>')
+                elm.append('<td colspan="' + str(span) + '">' + gen_div(col) + '</td>')
             else:
-                elm.append('<td>' + col + '</td>')
+                elm.append('<td>' + gen_div(col) + '</td>')
         table += gen_row("".join(elm))
 
     table += '</tbody></table>'
     return mark_safe(table)
 
 def gen_row(value):
-    return '<tr>' + value + '</tr>'
+    return '<tr>' + value + '</tr>\n'
+
+def gen_div(value):
+    import re
+
+    sharps_count = 0
+    for v in value:
+        print(v + ', ' + str(sharps_count))
+        if v == '#':
+            sharps_count += 1
+        else:
+            break
+    if sharps_count > 0:
+        return '<div class="ps' + str(sharps_count) + '">' + re.sub(r'^#+', '', value) + '</div>'
+    else:
+        return value
