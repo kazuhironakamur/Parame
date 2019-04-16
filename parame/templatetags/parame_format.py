c9004dfe4c9779b54d0b3ba8a80a8ccd945b8b8d
Kazuhiro Nakamura <kazuhironakamur@yahoo.co.jp>
2019-04-16 22:32:04 +0900

    ちょっと読みやすく改修

--- parame/templatetags/parame_format.py
+++ parame/templatetags/parame_format.py
@@ -10,44 +10,59 @@ https://djangoproject.jp/doc/ja/1.0/howto/custom-template-tags.html
 """
 @register.filter
 def tab(value):
-    esc = conditional_escape
 
-    max_cols_count = 0
-    for line in esc(value).splitlines():
-        cols_count = len(line.split('|'))
-        if max_cols_count < cols_count:
-            max_cols_count = cols_count
+    max_col_count = confirm_max_col(value)
 
     table = '<table class="table table-sm table-bordered table-hover">\n'
-    table += '<thead class="thead-light"><th colspan="' + str(max_cols_count) +  '">Parameter Sheet</th><thead>\n'
+    table += '<thead class="thead-light"><th colspan="' + str(max_col_count) +  '">Parameter Sheet</th><thead>\n'
     table += '<tbody>\n'
-    for line in esc(value).splitlines():
-        cols_count = len(line.split('|'))
-        elm = []
-        for col in line.split('|'):
-            span = max_cols_count - cols_count + 1
-            if span > 1:
-                elm.append('<td colspan="' + str(span) + '">' + gen_div(col) + '</td>')
-            else:
-                elm.append('<td>' + gen_div(col) + '</td>')
-        table += gen_row("".join(elm))
+    for line in value.splitlines():
+        table += parse_row(line, max_col_count)
 
     table += '</tbody></table>'
     return mark_safe(table)
 
-def gen_row(value):
-    return '<tr>' + value + '</tr>\n'
+def confirm_max_col(value):
+    max_col_count = 0
+    for line in value.splitlines():
+        cols_count = len(line.split('|'))
+        if max_col_count < cols_count:
+            max_col_count = cols_count
+    return max_col_count
 
-def gen_div(value):
-    import re
-
-    sharps_count = 0
-    for v in value:
+def confirm_sharp_count(line):
+    sharp_count = 0
+    for v in line:
         if v == '#':
-            sharps_count += 1
+            sharp_count += 1
         else:
             break
-    if sharps_count > 0:
-        return '<div class="ps' + str(sharps_count) + '">' + re.sub(r'^#+', '', value) + '</div>'
-    else:
-        return value
+    return sharp_count
+
+def parse_row(line, max_col_count):
+    import re
+
+    esc = conditional_escape
+    
+    sharp_count = confirm_sharp_count(line)
+    line = re.sub(r'^#+', '', line)
+    
+    split_line = line.split('|')
+    cols_count = len(split_line)
+    elm = []
+    for col in split_line:
+        pan = max_col_count - cols_count + 1
+        
+        if sharp_count > 0 and pan > 1:
+            elm.append('<td colspan="' + str(pan) + '" class="ps' + str(sharp_count) + '">')
+        elif pan > 1:
+            elm.append('<td colspan="' + str(pan) + '">')
+        elif sharp_count > 0:
+            elm.append('<td class="ps' + str(sharp_count) + '">')
+        else:
+            elm.append('<td>')
+        
+        elm.append(esc(col))
+        elm.append('</td>')
+    
+    return '<tr>' + "".join(elm) + '</tr>\n'
