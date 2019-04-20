be5ad3d36ea03fb02ee89c85c3e6a536d510ad24
Kazuhiro Nakamura <kazuhironakamur@yahoo.co.jp>
2019-04-20 22:10:39 +0900

    project編集画面を追加。

--- parame/urls.py
+++ parame/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.project_list, name='project_list'),
+    path('project/<int:pk>/edit', views.project_edit, name='project_edit'),
     path('<int:pk>/sheets', views.sheet_list, name='sheet_list'),
     path('<int:pk>/sheets/<int:sk>', views.sheet_detail, name='sheet_detail'),
     path('<int:pk>/sheets/<int:sk>/edit', views.sheet_edit, name='sheet_edit'),
--- parame/views.py
+++ parame/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .forms import SheetForm
+from .forms import ProjectForm, SheetForm
 from .models import Project, Sheet
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
@@ -9,6 +9,21 @@ from django.contrib.auth.models import User
 def project_list(request):
     projects = Project.objects.filter(owner=request.user.id)
     return render(request, 'parame/project/list.html', {'projects': projects})
+
+def project_edit(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    project.owner = request.user.id
+    if request.method == "POST":
+        form = ProjectForm(request.POST, instance=project)
+        print(form)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.save()
+            #return redirect('project_detail', pk=project.project)
+            return redirect('project_list')
+    else:
+        form = ProjectForm(instance=project)
+    return render(request, 'parame/project/edit.html', {'form': form})
 
 def sheet_list(request, pk):
     sheets = Sheet.objects.filter(project=pk)
